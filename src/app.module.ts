73e6ca90395e01cc5082f0550c9a72e9bd8180d4
Thalles Luz de Aguiar <thallesaguiar07@gmail.com>
2025-01-13 16:55:15 -0300

    config connection mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreditCardModule } from './credit-card/credit-card.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'docker',
+      database: 'creditcard',
+      entities: [],
+      synchronize: true,
+    }),
+    CreditCardModule,
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
